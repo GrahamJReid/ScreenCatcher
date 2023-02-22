8e2651173322d6d931245375e7a33fc1c84cc3b2
GrahamJReid <grahamreidtechnology@gmail.com>
2023-02-22 11:38:39 -0600

    created FoldersPage.module.css

--- components/imagesPage/ImagesPageContent.js
+++ components/imagesPage/ImagesPageContent.js
@@ -6,7 +6,7 @@ export default function ImagesPageContent() {
   return (
     <div id="gallery" className={styles.PageContentDivContainer}>
       <div className={styles.PageContentContainer}>
-        <h1 className={imagepagestyles.Title}>Images</h1>
+        <h1 className={imagepagestyles.ContentTitle}>Images</h1>
         <input type="text" placeholder="search" />
       </div>
     </div>
