f2e32b18ac813187e5c2f10530dc0df2259b94e0
GrahamJReid <grahamreidtechnology@gmail.com>
2023-02-21 21:34:37 -0600

    created ImagePageHero

--- pages/images.js
+++ pages/images.js
@@ -1,21 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import styles from '../styles/ImagesPage.module.css';
-import { useAuth } from '../utils/context/authContext';
+import ImagePageHero from '../components/ImagePageHero';
 
 export default function Images() {
-  const { user } = useAuth();
   return (
-    <div id="home" className={styles.HeroDivContainer}>
-      {/* overlay */}
-      <div className="hero-overlay" />
-      <div className="hero-content-container">
-        <img src={user.photoURL} height="200" width="200" />
-        <h2 className="hero-heading">{user.displayName}</h2>
-        <p className="hero-message"> Start Catching Images</p>
+    <>
+      <div className="home-div-container">
+        <ImagePageHero />
       </div>
-
-    </div>
+    </>
   );
 }
