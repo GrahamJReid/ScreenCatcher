58423ab9ab5717c6057c46d4e6d348324607dabf
GrahamJReid <grahamreidtechnology@gmail.com>
2023-02-22 11:32:24 -0600

    created ImagesPage.module.css

--- components/imagesPage/ImagesPageContent.js
+++ components/imagesPage/ImagesPageContent.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from '../../styles/ImagesFoldersPageLayout.module.css';
+import imagepagestyles from '../../styles/ImagesPage.module.css';
 
 export default function ImagesPageContent() {
   return (
     <div id="gallery" className={styles.PageContentDivContainer}>
       <div className={styles.PageContentContainer}>
-        <h1 className="gallery-title">Gallery</h1>
+        <h1 className={imagepagestyles.Title}>Images</h1>
         <input type="text" placeholder="search" />
       </div>
     </div>
